e882c62342c094ca830973096142163e4c61e106
Ken Sato <ksato9700@gmail.com>
2011-03-10 08:37:33 -0800

    reimplemented using pycurl

--- pydlc/client.py
+++ pydlc/client.py
@@ -1,51 +1,67 @@
 #
 # Copyright 2011 Kenichi Sato <ksato9700@gmail.com>
 #
+import os
 import urlparse
-import urllib2
-import httplib
 import datetime
 
+import pycurl
 import dns.resolver
 
 
-class NP_HTTPConnection(httplib.HTTPConnection):
-    def getresponse(self):
-        return super(NP_HTTPConnection, self).getresponse(buffering=False)
-
-class NB_HTTPHandler(urllib2.HTTPHandler):
-    def http_open(self, req):
-        return self.do_open(NB_HTTPConnection, req)
-
-BUFSIZ = 4096*1024
-
 class Client(object):
+
+    def progress(self, download_t, download_d, upload_t, upload_d):
+        ts = (datetime.datetime.utcnow(), download_d)
+        self.tslist.append(ts)
+
     def __init__(self, url):
         self.urlobj = urlparse.urlparse(url)
         self.addresses = map(lambda rdata: getattr(rdata, 'address'), 
                              dns.resolver.query(self.urlobj.hostname, 'A'))
+        self.c = pycurl.Curl()
+        self.c.setopt(pycurl.NOPROGRESS, 0)
+        self.c.setopt(pycurl.PROGRESSFUNCTION, self.progress)
+        self.c.setopt(pycurl.HTTPHEADER, ["Host:"+self.urlobj.hostname])
+        #self.c.setopt(pycurl.VERBOSE, 1)
+        self.c.setopt(pycurl.WRITEDATA, open(os.devnull,"w"))
 
     def download(self):
         for address in self.addresses:
             netloc = address + (":" + self.urlobj.port if self.urlobj.port else "")
             newurl = urlparse.urlunparse(self.urlobj[:1] + (netloc,) + self.urlobj[2:])
-            print newurl
-            request = urllib2.Request(newurl, headers={'Host':self.urlobj.hostname})
-            fp = urllib2.urlopen(request)
-            tslist = []
-            tslist.append((0, datetime.datetime.utcnow()))
-            while(True):
-                print '.'
-                buf = fp.read(BUFSIZ)
-                l = len(buf)
-                tslist.append((l,datetime.datetime.utcnow()))
-                if not l:
-                    break
-
-            for i in range(len(tslist)-1):
-                print tslist[i+1][0] * 8 / (tslist[i+1][1] - tslist[i][1]).total_seconds() / 1000.0 / 1000.0 
             
+            self.c.setopt(pycurl.URL, newurl)
+            self.tslist = []
+            self.c.perform()
 
+            print "Average speed: %0.2f Mbps" % (self.c.getinfo(pycurl.SPEED_DOWNLOAD)*8/1024.0/1024.0)
+
+            #print self.c.getinfo(pycurl.NAMELOOKUP_TIME)
+            #print self.c.getinfo(pycurl.CONNECT_TIME)
+            #print self.c.getinfo(pycurl.APPCONNECT_TIME)
+            #print self.c.getinfo(pycurl.PRETRANSFER_TIME)
+            #print self.c.getinfo(pycurl.STARTTRANSFER_TIME)
+            #print self.c.getinfo(pycurl.REDIRECT_TIME)
+            print "Total time: %0.2f sec" % self.c.getinfo(pycurl.TOTAL_TIME)
+
+            llen = len(self.tslist)
+            step =  llen/10
+            
+            #for i in range(llen-15, llen):
+            #    print self.tslist[i]
+
+            try :
+                for i in range (0, llen, step):
+                    tdelta = self.tslist[i+step][0] - self.tslist[i][0]
+                    ddelta = self.tslist[i+step][1] - self.tslist[i][1]
+                    tpos = self.tslist[i+step][0] - self.tslist[0][0] + tdelta/2
+                    print "%0.2f Mbps @ %0.2f" % (ddelta*8/tdelta.total_seconds()/1024.0/1024.0, tpos.total_seconds())
+
+            except IndexError:
+                pass
+
+            break
 
 def main():
     import sys
